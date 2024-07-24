19c147031e6b9a64a96213bf989d8b46ee1c39e3
Bhavjot121 <bh4374719@alphacollege.me>
2024-07-24 17:10:16 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class MyApp:
         self.documents = []
         self.embeddings = None
         self.index = None
-        self.load_pdf("THEDIA1.pdf")
+        self.load_pdf("Canadian Tourism.pdf")
         self.build_vector_db()
 
     def load_pdf(self, file_path: str) -> None:
@@ -53,7 +53,7 @@ def respond(
     temperature: float,
     top_p: float,
 ):
-    system_message = "You are a knowledgeable DBT coach. You always talk about one options at at a time. you add greetings and you ask questions like real counsellor. Remember you are helpful and a good listener. You are concise and never ask multiple questions, or give long response. You response like a human counsellor accurately and correctly. consider the users as your client. and practice verbal cues only where needed. Remember you must be respectful and consider that the user may not be in a situation to deal with a wordy chatbot.  You Use DBT book to guide users through DBT exercises and provide helpful information. When needed only then you ask one follow up question at a time to guide the user to ask appropiate question. You avoid giving suggestion if any dangerous act is mentioned by the user and refer to call someone or emergency."
+    system_message = "Given the ongoing evolution of factors impacting travel, Destination Canada (DC), in collaboration with Tourism Economics, a subsidiary of Oxford Economics, has crafted a revised Tourism Outlook extending to 2030"
     messages = [{"role": "system", "content": system_message}]
 
     for val in history:
@@ -85,22 +85,19 @@ demo = gr.Blocks()
 
 with demo:
     gr.Markdown(
-        "‼️Disclaimer: This chatbot is based on a DBT exercise book that is publicly available. and just to test RAG implementation.‼️"
+        
     )
     
     chatbot = gr.ChatInterface(
         respond,
         examples=[
-            ["I feel overwhelmed with work."],
-            ["Can you guide me through a quick meditation?"],
-            ["How do I stop worrying about things I can't control?"],
-            ["What are some DBT skills for managing anxiety?"],
-            ["Can you explain mindfulness in DBT?"],
-            ["I am interested in DBT excercises"],
-            ["I feel restless. Please help me."],
-            ["I have destructive thoughts coming to my mind repetatively."]
+            ["Explain The Opportunity Gap in Canada?"],
+            ["What is the value of Tourism in Canada?"],
+            ["How Canada, tourism, post recovery, is expected to grow faster than the general economy?"],
+            ["How Tourism remains resilient amidst global challenges?"],
+            ["How Domestic activity led revenue recovery, but international is ready to outpace growth trajectory?"],
         ],
-        title='Dialectical Behaviour Therapy Assistant👩‍⚕️🧘‍♀️'
+        title='Tourism in Canada'
     )
 
 if __name__ == "__main__":
